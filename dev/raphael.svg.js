39f040344c7ff7ce2dc3e3e76a1a95ddcf78eada
hbarudin <hbarudin@gmail.com>
2016-04-24 13:36:43 -0400

    Fix bug where fontSize is NaN

--- dev/raphael.svg.js
+++ dev/raphael.svg.js
@@ -584,6 +584,9 @@ define(["./raphael.core"], function(R) {
             node = el.node,
             fontSize = node.firstChild ? toInt(R._g.doc.defaultView.getComputedStyle(node.firstChild, E).getPropertyValue("font-size"), 10) : 10;
 
+        // Address bug where fontSize is NaN
+        if (isNaN(fontSize)) fontSize = toInt(a['font-size'], 10);
+
         if (params[has]("text")) {
             a.text = params.text;
             while (node.firstChild) {
